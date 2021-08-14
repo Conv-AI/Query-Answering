cc2d1ca7b795588cc1e9918c6671b92e507e8647
Himadri Sankar Chatterjee <himadri.tito13@gmail.com>
2021-08-14 21:06:12 +0530

    Added try-except for better experience

--- query_anwering_api.py
+++ query_anwering_api.py
@@ -59,26 +59,28 @@ def getRestrictedAnswer():
 
 @app.route("/query_answer", methods=["POST"])
 def getAnswer():
-    data = request.get_json()
-    genQA_flag = data["use_ans_extender"]
-    start_time = time.time()
-    for url in list(mg.search_url(query=data["question"])):
-        result = trafilatura.extract(trafilatura.fetch_url(url))
-        payload = {"question": data["question"],
-                   "input_context": result, }
-        response = requests.request(
-            "POST", qa_url, headers=headers, json=payload, verify=False)
-        response = response.json()
-        if len(response["result"]) > 0:
-            payload = {
-                "question": data["question"],
-                "input_context": response["result"]
-            }
+    try:
+        data = request.get_json()
+        genQA_flag = data["use_ans_extender"]
+        start_time = time.time()
+        for url in list(mg.search_url(query=data["question"])):
+            result = trafilatura.extract(trafilatura.fetch_url(url))
+            payload = {"question": data["question"],
+                       "input_context": result, }
             response = requests.request(
-                "POST", genqa_url, headers=headers, json=payload, verify=False)
-            return json.dumps(response.json())
-
-    return (json.dumps({"result": "Sorry. I don't have an answer to that.", "context": "No possible context for the given query", "p": 1.0}))
+                "POST", qa_url, headers=headers, json=payload, verify=False)
+            response = response.json()
+            if len(response["result"]) > 0:
+                payload = {
+                    "question": data["question"],
+                    "input_context": response["result"]
+                }
+                response = requests.request(
+                    "POST", genqa_url, headers=headers, json=payload, verify=False)
+                return json.dumps(response.json())
+    
+    except:
+        return (json.dumps({"result": "Sorry. I don't have an answer to that.", "context": "No possible context for the given query", "p": 1.0}))
 
 
 if __name__ == "__main__":
